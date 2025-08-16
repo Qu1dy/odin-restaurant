16a10c397ea67e3a8eeb2d0ea85d09b43f3417c8
Qu1dy <71177869+Qu1dy@users.noreply.github.com>
2025-08-16 18:49:05 +0300

    refactor: menu structure

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,28 +1,49 @@
-import coffeeImg from "../assets/coffee.jpg";
+class Category {
+    constructor(name) {
+        this.node = document.createElement("div");
+        this.node.classList.add("category");
+        this.seperationLine = document.createElement("hr");
+        this.seperationLine.classList.add("line");
+        const h1 = document.createElement("h1");
+        h1.innerText = name;
+        this.node.appendChild(h1);
+        this.node.appendChild(this.seperationLine.cloneNode());
+    }
+
+    addItem(name, desc) {
+        const itemName = document.createElement("h2");
+        itemName.innerText = name;
+        const itemDesc = document.createElement("p");
+        itemDesc.innerText = ` - ${desc}`;
+        itemDesc.style.display = itemName.style.display = "inline";
+        this.node.appendChild(itemName);
+        this.node.appendChild(itemDesc);
+
+        const lineBreak = document.createElement("br");
+        this.node.appendChild(lineBreak);
+    }
+
+    get el() {
+        this.node.appendChild(this.seperationLine);
+        return this.node;
+    }
+}
 
 const menuPage = document.createElement("div");
 menuPage.classList.add("menupage");
 
-const addCard = (itemTitle, itemDesc, imgSrc) => {
-    const card = document.createElement("div");
-    const img = document.createElement("img");
-    img.src = imgSrc;
-    img.width = 80;
-    const title = document.createElement("h1");
-    title.innerText = itemTitle;
+const title = document.createElement("h1");
+title.innerText = "Our menu";
 
-    const desc = document.createElement("p");
-    desc.innerText = itemDesc;
+const drinks  = new Category("Drinks");
+drinks.addItem("Purrfect Latte", "classic latte with cat latte art");
+drinks.addItem("Meowcchiato", "caramel macchiato with a paw-print drizzle");
 
-    card.appendChild(img);
-    card.appendChild(title);
-    card.appendChild(desc);
-    card.classList.add("card");
-    menuPage.appendChild(card);
-}
+const desserts = new Category("Desserts");
+desserts.addItem("Pawprint Cookies", "shortbread with chocolate paw design");
 
-for(let i =0;i<12;i++) {
-    addCard("Pawfect Latte", "Velvety mocha with playful cat-themed foam art.",coffeeImg);
-}
+menuPage.appendChild(title);
+menuPage.appendChild(drinks.el);
+menuPage.appendChild(desserts.el);
 
 export default menuPage;
