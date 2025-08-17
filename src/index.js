2b0e8ab241c1878ec49abd55c552db58385becc1
Qu1dy <71177869+Qu1dy@users.noreply.github.com>
2025-08-17 22:40:55 +0300

    fix(homepage): button not updating nav underline

--- src/index.js
+++ src/index.js
@@ -4,55 +4,55 @@ import menu from "./modules/menu";
 import contact from "./modules/contact";
 
 const pageController = (() => {
-    let content = document.querySelector("#content"), buttons;
-
-    const buttonNameToPage = {"home": home, "menu": menu, "contact": contact};
-
-    const _cacheDom = () => {
-        buttons = document.querySelectorAll("button");
-    }
+    let content = document.querySelector("#content"),
+        buttons = document.querySelectorAll("header button"),
+        activeButton = buttons[0];
+    const buttonNameToPage = { "home": home, "menu": menu, "contact": contact };
+    let homepageMenuButton;
 
     const init = () => {
         _changePage(home);
-        _cacheDom();
+        homepageMenuButton = document.querySelector(".homepage button");
         _handleEvents();
-    }
+    };
 
     const _getPage = (button) => {
         const moduleName = button.innerText.toLowerCase();
         const page = buttonNameToPage[moduleName];
         return page;
-    }
-
-    const _addActiveClass = (button) => {
-        button.classList.toggle("active");
-    }
-
-    const _removeActiveFromButtons = () => {
-        buttons.forEach(button => {
-            if(button.classList.contains('active')) {
-                button.classList.toggle('active');
-            }
-        });
-    }
+    };
 
     const _changePage = (page) => {
         content.innerHTML = "";
         content.appendChild(page);
-    }
+    };
 
-    const _handleEvents = () => {
+    const _handleNavButtons = () => {
         buttons.forEach(button => {
-            button.addEventListener("click", () => 
-                {
-                    _changePage(_getPage(button));
-                    _removeActiveFromButtons();
-                    _addActiveClass(button);
-                });
+            button.addEventListener("click", () => {
+                console.log(button.parentElement);
+                activeButton.classList.toggle("active");
+                activeButton = button;
+                activeButton.classList.toggle("active");
+                _changePage(_getPage(button));
+            });
         });
     };
 
-    return {init};
+    const _handleHPMenuButton = () => {
+        homepageMenuButton.addEventListener("click", () => {
+            const navMenuButton = Array.from(buttons).find(button => button.textContent === "Menu");
+            const ev = new Event("click");
+            navMenuButton.dispatchEvent(ev);
+        });
+    };
+
+    const _handleEvents = () => {
+        _handleNavButtons();
+        _handleHPMenuButton();
+    };
+
+    return { init };
 })();
 
 pageController.init();
